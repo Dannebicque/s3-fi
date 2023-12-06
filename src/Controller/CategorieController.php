d72b8afd0d7bae510979245695c2e999d16b2299
davidannebicque <david.annebicque@gmail.com>
2023-12-06 17:49:26 +0100

    Formulaire sur Catégorie

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -3,12 +3,37 @@
 namespace App\Controller;
 
 use App\Entity\Categorie;
+use App\Form\CategorieType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CategorieController extends AbstractController
 {
+    #[Route('/categorie/ajout', name: 'app_categorie_ajout')]
+    public function ajout(
+        Request                $request,
+        EntityManagerInterface $entityManager
+    ): Response
+    {
+        $categorie = new Categorie();
+        $form = $this->createForm(CategorieType::class, $categorie);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            $entityManager->persist($categorie);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_categorie', ['categorie' => $categorie->getId()]);
+        }
+
+        return $this->render('categorie/ajout.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
     #[Route('/categorie/{categorie}', name: 'app_categorie')]
     public function index(Categorie $categorie): Response
     {
@@ -16,4 +41,6 @@ class CategorieController extends AbstractController
             'categorie' => $categorie,
         ]);
     }
+
+
 }
